30d0909a3573acac470e7f7d5e58ec32a5adc231
Simon Billinge <sbillinge@users.noreply.github.com>
2024-07-30 06:55:44 -0400

    Copyright (#32)
    * requirements
    
    * basefunction
    
    * all the copyright statements

--- diffpy/srmise/modelparts.py
+++ diffpy/srmise/modelparts.py
@@ -2,7 +2,8 @@
 ##############################################################################
 #
 # SrMise            by Luke Granlund
-#                   (c) 2014 trustees of the Michigan State University.
+#                   (c) 2014 trustees of the Michigan State University
+#                   (c) 2024 trustees of Columia University in the City of New York
 #                   All rights reserved.
 #
 # File coded by:    Luke Granlund
