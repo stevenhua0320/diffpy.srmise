9ecf5e8a970921721ca415abe248dee24f440e07
Rundong Hua <157993340+stevenhua0320@users.noreply.github.com>
2024-07-31 17:09:02 +0800

    move diffpy files to src dir (#44)
    * move diffpy files to src dir
    
    * [pre-commit.ci] auto fixes from pre-commit hooks
    
    * add Luke to authors
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/diffpy/srmise/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.srmise/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""Peak extraction and peak fitting tool for atomic pair distribution functions."""
+
+# package version
+from diffpy.srmise.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
