82888bb8a91d4acac8204fc056ab03c15fd72741
stevenhua0320 <r.hua@mail.utoronto.ca>
2024-08-08 02:03:28 +0800

    add one more case and change reference name to actual

--- src/diffpy/srmise/tests/test_dataclusters.py
+++ src/diffpy/srmise/tests/test_dataclusters.py
@@ -25,19 +25,37 @@ from diffpy.srmise.dataclusters import DataClusters
                 "status": 0,
             },
         ),
+        (
+            {
+                "input_x": np.array([1]),
+                "input_y": np.array([3]),
+                "input_res": 4,
+            },
+            {
+                "x": np.array([]),
+                "y": np.array([]),
+                "data_order": np.array([], dtype=np.int32),
+                "clusters": np.array([[]], dtype=np.int32),
+                "res": 0,
+                "current_idx": 0,
+                "lastcluster_idx": None,
+                "lastpoint_idx": None,
+                "status": 0,
+            },
+        ),
     ],
 )
 def test_clear(inputs, expected):
     # Initialize DataClusters with input parameters
-    c1 = DataClusters(x=inputs["input_x"], y=inputs["input_y"], res=inputs["input_res"])
+    actual = DataClusters(x=inputs["input_x"], y=inputs["input_y"], res=inputs["input_res"])
 
     # Perform the clear operation
-    c1.clear()
+    actual.clear()
 
     # Assert each expected attribute against its actual value after clearing
     for attr, expected_value in expected.items():
         assert (
-            np.array_equal(getattr(c1, attr), expected_value)
+            np.array_equal(getattr(actual, attr), expected_value)
             if isinstance(expected_value, np.ndarray)
-            else getattr(c1, attr) == expected_value
+            else getattr(actual, attr) == expected_value
         )
