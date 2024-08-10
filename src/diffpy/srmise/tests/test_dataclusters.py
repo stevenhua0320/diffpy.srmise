5aad8bd43fb50e27f41b1d275bdfb304cb40ca5c
Simon Billinge <sbillinge@users.noreply.github.com>
2024-08-10 17:10:14 -0400

    Eq tests (#59)
    * remove diffpy/srmise tree
    
    * test for eq

--- src/diffpy/srmise/tests/test_dataclusters.py
+++ src/diffpy/srmise/tests/test_dataclusters.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 import numpy as np
 
 from diffpy.srmise.dataclusters import DataClusters
@@ -10,3 +12,23 @@ def test_clear():
     # Perform the clear operation
     actual.clear()
     assert actual == expected
+
+
+def test___eq__():
+    actual = DataClusters(np.array([1, 2, 3]), np.array([3, 2, 1]), 0)
+    expected = DataClusters(np.array([1, 2, 3]), np.array([3, 2, 1]), 0)
+    assert expected == actual
+    attributes = vars(actual)
+    for attr_key, attr_val in attributes.items():
+        reset = copy(attr_val)
+        assert expected == actual
+        if attr_val is not None:
+            attributes.update({attr_key: attr_val + 1})
+        else:
+            attributes.update({attr_key: 1})
+        try:
+            assert not expected == actual
+        except AssertionError:
+            print(f"not-equal test failed on {attr_key}")
+            assert not expected == actual
+        attributes.update({attr_key: reset})
